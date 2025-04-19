27d39b5c9258260d85aa8470269bbee3e8d9c8a1
kamiye <kamiye31@gmail.com>
2025-04-20 00:36:40 +0100

    update Hero.jsx
    kamiye oladipo to kilometers

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
         <div className="w-full lg:w-1/2">
         <div className="flex flex-col items-center lg:items-start mt-10">
             <h2 className="pb-2 text-4xl tracking-tighter lg:text-8xl">
-                Kamiye Oladipo
+                Kilometers
             </h2>
             <span 
             className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent"> 
